9c465e96d066fa5ccec9ccd3a903cbc1f88667a6
Nenad <nbursa@gmail.com>
2024-04-05 21:10:53 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import { Router } from "./router";
+import Main from "./pages/main.html?raw";
+import "./style.scss";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = Main;
+
+const routes = [
+  { path: "/", component: () => import("./pages/home.html?raw") },
+  {
+    path: "/catalog",
+    component: () => import("./pages/catalog.html?raw"),
+    children: [
+      {
+        path: "/creation/constructor",
+        component: () => import("./pages/constructor.html?raw"),
+      },
+      {
+        path: "/creation/module",
+        component: () => import("./pages/module.html?raw"),
+      },
+      {
+        path: "/creation/revealing-module",
+        component: () => import("./pages/revealing-module.html?raw"),
+      },
+      {
+        path: "/creation/singleton",
+        component: () => import("./pages/singleton.html?raw"),
+      },
+      {
+        path: "/creation/prototype",
+        component: () => import("./pages/prototype.html?raw"),
+      },
+      {
+        path: "/creation/factory",
+        component: () => import("./pages/factory.html?raw"),
+      },
+      {
+        path: "/creation/abstract-factory",
+        component: () => import("./pages/abstract-factory.html?raw"),
+      },
+    ],
+  },
+];
+
+const router = new Router(routes);
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target instanceof HTMLElement) {
+    const href = target.getAttribute("href");
+    if (href && href.startsWith("/")) {
+      event.preventDefault();
+      router.navigate(href);
+    }
+  }
+});
+
+router.resolve();
---
+++ src/router.ts
@@ -0,0 +1,82 @@
+import NotFound from "./pages/not-found.html?raw";
+
+export class Router {
+  routes: {
+    path: string;
+    component: () => Promise<{ default: string }>;
+    children?: {
+      path: string;
+      component: () => Promise<{ default: string }>;
+    }[];
+  }[] = [];
+
+  constructor(
+    routes: {
+      path: string;
+      component: () => Promise<{ default: string }>;
+      children?: {
+        path: string;
+        component: () => Promise<{ default: string }>;
+      }[];
+    }[]
+  ) {
+    this.initRoutes(routes);
+  }
+
+  initRoutes(
+    routes: {
+      path: string;
+      component: () => Promise<{ default: string }>;
+      children?: {
+        path: string;
+        component: () => Promise<{ default: string }>;
+      }[];
+    }[]
+  ): void {
+    routes.forEach((route) => {
+      this.addRoute(route.path, route.component);
+
+      route.children?.forEach((child) => {
+        this.addRoute(`${route.path}${child.path}`, child.component);
+      });
+    });
+  }
+
+  addRoute(path: string, component: () => Promise<{ default: string }>): void {
+    if (!this.routes.some((route) => route.path === path)) {
+      this.routes.push({ path, component });
+    }
+  }
+
+  async navigate(path: string) {
+    window.history.pushState({}, "", path);
+    await this.resolve();
+  }
+
+  async resolve(): Promise<void> {
+    const path = window.location.pathname;
+    const route = this.routes.find((route) => route.path.startsWith(path));
+
+    const el =
+      document.querySelector("#router-view") || this.createRouterView();
+
+    if (route) {
+      try {
+        const componentModule = await route.component();
+        el.innerHTML = componentModule.default;
+      } catch (error) {
+        el.innerHTML = `Failed to load the component, ${error}`;
+        // el.innerHTML = NotFound;
+      }
+    } else {
+      el.innerHTML = NotFound;
+    }
+  }
+
+  createRouterView(): HTMLElement {
+    const el = document.createElement("div");
+    el.id = "router-view";
+    document.querySelector("#app")?.appendChild(el);
+    return el;
+  }
+}
