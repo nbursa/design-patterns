32d2430c92574b1186ad234de7bef3d00a780754
Nenad <nbursa@gmail.com>
2024-05-21 17:42:22 +0200

    Implement analytics

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-    base: '/'
+  base: '/',
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          VITE_GA_TRACKING_ID: import.meta.env.VITE_GA_TRACKING_ID,
+        },
+      },
+    }),
+  ],
 });
